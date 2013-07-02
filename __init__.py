1daff1e05198fb0c228039d42352a07c6191bd21
Danilo Bargen <gezuru@gmail.com>
2013-07-02 19:28:25 +0200

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,1 @@
+raise ImportError('"Please don\'t use star imports." -- Daniel Greenfeld, EuroPython 2013')
---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='star',
+      version=0.1,
+      description='import star',
+      long_description='Making star imports possible. Or not.',
+      author='Danilo Bargen',
+      author_email='gezuru@gmail.com',
+      license='Public Domain',
+      keywords='star import',
+      platforms=['any'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+          'Topic :: Software Development',
+          'Topic :: Software Development :: Quality Assurance',
+          ],
+    )
